5e80408b082a0b67c59ee4f5f1c331e3f5d884b2
Vanadium <vanadium@VanadiumdeMacBook-Air.local>
2024-10-17 16:38:13 +0800

    fix file structure, add README, move .md to /docs/

---
+++ docs/interface_defination.md
@@ -0,0 +1,34 @@
+To any heap class there, using the interface defination
+
+take binomial heap class for example, all heap class support following interface
+
+1. biuld an empty heap
+
+```C++
+auto binomial_heap = new BinomialHeap<T>;
+```
+
+2. insert an elememt
+
+```C++
+T key;
+binomial_heap<T>.insert(key);
+```
+
+3. find the pointer to the node storing the min elememt
+
+```C++
+auto t =  binomial_heap<T>.find_min();
+```
+
+4. delete the node storing the min elememt
+
+```C++
+binomial_heap<T>.delete_min();
+```
+
+5. check out if a key is existing
+
+```C++
+bool is_exist = binomial_heap<T>.is_exist(key);
+```
---
+++ src/code/Heap.hpp
@@ -0,0 +1,31 @@
+#include <queue>
+
+template <class T>
+struct cmp{
+    bool operator()(const T& x, const T& y) const {
+        return x > y;
+    }
+};
+
+template <class T>
+struct Heap{
+    std::priority_queue<T, std::vector<T>, cmp<T> > heap;
+    void insert(T key){
+        heap.push(key);
+    }
+    T find_min(){
+        return heap.top();
+    }
+    void delete_min(){
+        heap.pop();
+    }
+    bool is_exist(T key){
+        std::vector<T> t = heap.__get_container();
+        for(auto i:t){
+            if(i == key){
+                return true;
+            }
+        }
+        return false;
+    }
+};
